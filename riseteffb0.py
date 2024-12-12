ae01453c75f7e535d3ccd175d41f2b84e789ed23
Lu'luatul Maknunah <125227561+LuluatulMaknunah21-048@users.noreply.github.com>
2024-12-13 00:54:02 +0700

    Update riseteffb0.py

--- riseteffb0.py
+++ riseteffb0.py
@@ -10,8 +10,8 @@ import pytz
 # Fungsi untuk mendownload model dari Google Drive
 @st.cache_resource
 def download_model():
-    model_url = "https://drive.google.com/uc?id=1--SKfN1wkzUGlreZD1nGV-lOKFMAh6d0pW"
-    output_path = "best_model.keras"
+    model_url = "https://drive.google.com/uc?id=1SKfN1wkzUGlreZD1nGV-lOKFMAh6d0pW"
+    output_path = "EFFICIENTNETECA.keras"
     if not os.path.exists(output_path):
         gdown.download(model_url, output_path, quiet=False)
     return output_path
